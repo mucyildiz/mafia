5f7e38ec2617830aafa6dd8aa536c5112e5206a7
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-10-02 16:16:15 -0400

    Allow CORS

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
