943c764e33cf79af0bb781803a88a1fc50894dc6
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-10-02 14:48:31 -0400

    Add test route to express app

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const express = require('express');
 
 const app = express();
 
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
