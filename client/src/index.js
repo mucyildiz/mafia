51ad6b0c644a86c50f1f0573a181205cd589e3d4
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-10-03 14:59:04 -0400

    Add Grommet wrapper

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Grommet } from 'grommet';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Grommet>
+      <App />
+    </Grommet>
   </React.StrictMode>,
   document.getElementById('root')
 );
