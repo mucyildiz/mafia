a37eab1d80353dba2890e430ac25648b77aa9c5b
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-10-03 18:30:03 -0400

    Update authRoute

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,24 +1,5 @@
-const { Client } = require('pg');
-const keys = require('../config/keys');
-
-const credentials = {
-  user: keys.username,
-  password: keys.password,
-  port: keys.port,
-  host: keys.host,
-  database: keys.database
-}
-
 module.exports = app => {
   app.get('/api/test', (req, res) => {
     res.send("Successful connection!")
   });
-
-  app.get('/api/test_db', async (req, res) => {
-    const client = new Client(credentials);
-    await client.connect();
-    const result = await client.query('SELECT * FROM users');
-    await client.end();
-    res.send(result.rows);
-  })
 }
