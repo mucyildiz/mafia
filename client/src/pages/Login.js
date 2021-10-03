da1be7aae8de2da7adff523d1c3827da5e67f5b3
Jeffrey Carr <jeff@Jeffs-Computer.local>
2021-10-03 19:30:20 -0400

    Created initial login page and temp 404 page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,5 @@
+import { Text } from 'grommet';
+
+export default function Login() {
+  <Text>This is going to be the login page.</Text>
+}
---
+++ client/src/pages/Oops.js
@@ -0,0 +1,5 @@
+import { Text } from 'grommet';
+
+export default function Oops() {
+    <Text>Oops, this is a 404 page.</Text>
+}
