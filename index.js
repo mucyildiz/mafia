ab1f131c44d3a7de3f2e1cc9bc2d88864cce56d1
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-10-03 18:29:19 -0400

    Connect to DB

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const keys = require('./config/keys');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(`postgres://${keys.username}:${keys.password}@${keys.host}:${keys.port}/${keys.database}`);
+sequelize.authenticate().then(() => {
+  console.log('Connection to database has been established successfully.');
+}).catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
 
 const app = express();
 
 require('./routes/authRoutes')(app);
+require('./routes/dbRoutes')(app, sequelize);
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'))
