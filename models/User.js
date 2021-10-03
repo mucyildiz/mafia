3ea44d5e96f82ea6d553e6a91e07c44a6aa5c146
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-10-03 19:09:56 -0400

    Refactor

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const { DataTypes } = require('sequelize');
+const db = require('../services/db');
 
-module.exports = sequelize => sequelize.define('user', {
+module.exports = db.define('user', {
   user_id: { type: DataTypes.STRING, primaryKey: true, allowNull: false },
   user_name: { type: DataTypes.STRING, allowNull: false },
   user_wins: { type: DataTypes.INTEGER, allowNull: false },
--- routes/dbRoutes.js
+++ routes/dbRoutes.js
@@ -1,11 +1,16 @@
-module.exports = (app, db) => {
-  const User = require('../models/User')(db);
+const User = require('../models/User');
+
+module.exports = (app) => {
   app.get('/api/test_db', async (req, res) => {
-    const users = await User.findAll({
-      where: {
-        user_id: '0'
-      }
+    const user = await User.findByPk('0');
+    res.send(user);
+  });
+
+  app.post('/api/user', async (req, res) => {
+    return User.create({
+      user_id: req.body.user_id,
+      user_name: req.body.user_name,
+      user_wins: req.body.user_wins
     });
-    res.send(users);
   });
 }
