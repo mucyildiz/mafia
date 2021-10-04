bacfa67a403e8c0aea9f25c1b0362f03695dd9a9
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-10-03 20:17:03 -0400

    Add routes for google auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,22 @@
+const passport = require('passport');
+
 module.exports = app => {
   app.get('/api/test', (req, res) => {
-    res.send("Successful connection!")
+    console.log(req.user);
   });
+
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile']
+    })
+  );
+
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect: process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3000/',
+        failureRedirect: process.env.NODE_ENV === 'production' ? "/" : "http://localhost:3000/"
+    }),
+  );
 }
