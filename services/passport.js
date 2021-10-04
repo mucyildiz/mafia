b92807ebfa5826aaf988604f22149cd448653246
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-10-03 20:17:36 -0400

    Configure passport

--- services/passport.js
+++ services/passport.js
@@ -1,3 +1,38 @@
-const { Client } = require('pg');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const User = require('../models/user');
+
+passport.serializeUser((user, done) => {
+  done(null, user.user_id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+    .then(user => {
+      done(null, user);
+    })
+});
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findByPk(profile.id);
+  
+    if(existingUser){
+      return done(null, existingUser);
+    }
+
+    const newUser = await User.create({
+      user_id: profile.id,
+      user_name: profile.name.givenName,
+      user_wins: 0
+    }).catch(err => console.log(err));
+
+    done(null, newUser);
+  })
+);
