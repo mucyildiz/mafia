f194ec1ccd8e56f3d977f180a3277cef88a8813f
Alperen Yildiz <alperenyildiz21@gmail.com>
2021-10-03 20:17:31 -0400

    Add DB endpoints

--- routes/dbRoutes.js
+++ routes/dbRoutes.js
@@ -7,10 +7,17 @@ module.exports = (app) => {
   });
 
   app.post('/api/user', async (req, res) => {
-    return User.create({
+    const existingUser = await User.findByPk(req.body.user_id);
+    if(existingUser){
+      console.log("User already exists.")
+      res.end();
+      return;
+    }
+    await User.create({
       user_id: req.body.user_id,
       user_name: req.body.user_name,
       user_wins: req.body.user_wins
-    });
+    }).catch(err => console.log(err));
+    res.end();
   });
 }
