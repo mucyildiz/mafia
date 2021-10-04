41eb0e32945c291d61aaf070cf553d232860761b
Jeffrey Carr <jeff@Jeffs-Computer.local>
2021-10-03 21:58:22 -0400

    Created basic login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,28 @@
-import { Text } from 'grommet';
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Text } from 'grommet';
+import { Google } from 'grommet-icons';
+
+import AppBar from '../components/AppBar';
 
 export default function Login() {
-  <Text>This is going to be the login page.</Text>
+  return(
+    <Box responsive align="center" full>
+      <AppBar />
+
+      <Box pad="xlarge" align="center">
+        <Card basis="small" align="center" elevation="large" background="main">
+          <CardHeader>
+            <Box pad="small">
+              <Text>Login</Text>
+            </Box>
+          </CardHeader>
+          <CardBody pad="medium">
+            <Text>To continue to the game, please log in with Google</Text>
+          </CardBody>
+          <CardFooter pad="medium">
+            <Button primary reverse label="Login with Google" icon={<Google color='plain' />} onClick={() => {alert("Signing in with Google")}} />
+          </CardFooter>
+        </Card>
+      </Box>
+    </Box>
+  );
 }
